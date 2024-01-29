1a5194080140c9a351ffd658438ce5bd57122a2d
Mohamad Irfan Islami <mohamad.islami@alodokter.com>
2024-01-29 19:41:39 +0700

    controller representers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotDestroyed, with: :not_destroyed
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   private
 
@@ -7,5 +8,9 @@ class ApplicationController < ActionController::API
     # s = File.open("others/errors.json").read
     # Rails.logger.info s
     render json: {errors: e.record.errors}, status: :unprocessable_entity
+  end
+
+  def not_found(e)
+    render json: {}, status: :not_found
   end
 end
